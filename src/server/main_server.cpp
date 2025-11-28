6cefd30e136f0ee2d740213e714e7bdd11bc4feb
patromi <patromi123@gmail.com>
2025-11-28 22:12:16 +0100

    add communication md architecture

--- src/server/main_server.cpp
+++ src/server/main_server.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
 
+class Server{
+
+
+  void Server(){
+
+  }  
+
+
+  
+  void run(){
+
+
+  }
+
+}
+
+
+
+
+
 int main(int, char**){
+
+  Server server = Server
+  server.run()
 
   std::cout << "Client is running!" << std::endl;
   return 0;
