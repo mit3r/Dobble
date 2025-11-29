e9e418c5f606f06cfad39337b58da44d458577a9
patromi <patromi123@gmail.com>
2025-11-30 00:16:50 +0100

    add a client-lobbyserver protocol

---
+++ src/protocol/CommandFactory.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <map>
+#include <functional>
+#include <string>
+#include "lobby/lobbyclient/SocketCommands.hpp"
+
+class CommandFactory {
+private:
+    using CreatorFunc = std::function<AnyCommand(const json&)>;
+    std::map<std::string, CreatorFunc> dictionary;
+
+    bool isResponse(const json& j) { //is_server determine its a response command
+        return j.contains("server_id") && !j["server_id"].is_null();
+    }
+
+public:
+    CommandFactory() {
+        
+        dictionary["ping"] = [this](const json& j) -> AnyCommand {
+            if (isResponse(j)) return j.get<ResponsePingCommand>();
+            else               return j.get<SenderPingCommand>();
+        };
+
+        dictionary["join_game"] = [this](const json& j) -> AnyCommand {
+            if (isResponse(j)) return j.get<ResponseJoinGameCommand>();
+            else               return j.get<SenderJoinGameCommand>();
+        };
+
+        dictionary["getinfolobby"] = [this](const json& j) -> AnyCommand {
+            if (isResponse(j)) return j.get<ResponseGetLobbyInfoCommand>();
+            else               return j.get<SenderGetLobbyInfoCommand>();
+        };
+
+        dictionary["leaveroom"] = [this](const json& j) -> AnyCommand {
+            if (isResponse(j)) return j.get<ResponseLeaveRoomCommand>();
+            else               return j.get<SenderLeaveRoomCommand>();
+        };
+
+        dictionary["sendgame_info"] = [this](const json& j) -> AnyCommand {
+            if (isResponse(j)) return j.get<ResponseSendGameInfoCommand>();
+            else               return j.get<SenderSendGameInfoCommand>();
+        };
+    }
+
+    AnyCommand get(const std::string& commandName, const json& j) {
+        if (dictionary.count(commandName)) {
+            return dictionary[commandName](j);
+        }
+        return std::monostate{};
+    }
+};
---
+++ src/protocol/lobby/lobbyclient/SocketCommands.hpp
@@ -0,0 +1,206 @@
+#pragma once
+#include <string>
+#include <optional>
+#include <list>
+#include <variant>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+namespace nlohmann {
+    template <typename T>
+    struct adl_serializer<std::optional<T>> {
+        static void to_json(json& j, const std::optional<T>& opt) {
+            if (opt == std::nullopt) j = nullptr; else j = *opt;
+        }
+        static void from_json(const json& j, std::optional<T>& opt) {
+            if (j.is_null()) opt = std::nullopt; else opt = j.get<T>();
+        }
+    };
+}
+
+struct ImageInfo {
+    std::string img_name;
+    std::optional<std::string> rotate;
+    std::optional<std::string> rotation;
+    std::optional<std::string> place;
+    std::optional<std::string> size;
+};
+
+struct PlayerGameInfo {
+    std::string client_id;
+    std::optional<std::list<ImageInfo>> imgs;
+    std::optional<int> score;
+    std::optional<int> points;
+    std::optional<int> mistakes;
+    std::optional<int> rank;
+};
+
+struct ScoreEntry {
+    std::string client_id;
+    std::string score;
+};
+
+struct PastTurn {
+    std::string turn_id;
+    std::optional<std::string> winner;
+    std::optional<std::string> winner_id;
+    std::optional<std::string> active;
+};
+
+struct TurnStruct {
+    std::string turn_id;
+    std::optional<bool> active;
+    std::optional<std::list<PlayerGameInfo>> clients_data;
+    std::optional<std::list<ImageInfo>> images_on_table;
+    std::optional<std::list<ScoreEntry>> scoreboard;
+    std::optional<std::list<PastTurn>> past_turns;
+    std::optional<std::string> winner_id;
+    std::optional<std::list<PlayerGameInfo>> clients_id_data;
+    std::optional<std::list<std::string>> actual_imgs;
+    std::optional<std::list<PlayerGameInfo>> players_imgs;
+};
+
+struct ActualGameStruct {
+    std::string game_id;
+    std::string game_name;
+    std::string players;
+    std::string max_players;
+    std::list<std::string> nicknames;
+    std::string status;
+};
+
+struct ShortGameInfo {
+    std::string game_id;
+    std::string name;
+};
+
+struct ErrorStruct {
+    std::optional<int> code;
+    std::string message;
+};
+
+class LobbyServerMessageCore {
+public:
+    std::string command;
+    std::optional<std::string> server_id; 
+    std::optional<std::string> client_id;
+    std::optional<std::string> game_id;
+    std::optional<ErrorStruct> error;
+};
+
+class SenderPingCommand : public LobbyServerMessageCore {
+public:
+    struct data { std::string message; };
+    std::optional<data> data_obj;
+};
+class ResponsePingCommand : public LobbyServerMessageCore {
+public:
+    struct data { std::string message; };
+    std::optional<data> data_obj;
+};
+
+class SenderGetLobbyInfoCommand : public LobbyServerMessageCore {
+public:
+    struct data { std::string page; };
+    std::optional<data> data_obj;
+};
+class ResponseGetLobbyInfoCommand : public LobbyServerMessageCore {
+public:
+    struct data {
+        std::string page;
+        std::list<ActualGameStruct> actual_games;
+        std::optional<std::string> next_page;
+    };
+    std::optional<data> data_obj;
+};
+
+class SenderJoinGameCommand : public LobbyServerMessageCore {
+public:
+    struct data { std::string game_id; std::string role; };
+    std::optional<data> data_obj;
+};
+class ResponseJoinGameCommand : public LobbyServerMessageCore {
+public:
+    struct data { std::string status; std::string role; ShortGameInfo game_info; };
+    std::optional<data> data_obj;
+};
+
+class SenderLeaveRoomCommand : public LobbyServerMessageCore {
+public:
+    struct data { std::string message; };
+    std::optional<data> data_obj;
+};
+class ResponseLeaveRoomCommand : public LobbyServerMessageCore {
+public:
+    struct data { std::string message; };
+    std::optional<data> data_obj;
+};
+
+class SenderSendGameInfoCommand : public LobbyServerMessageCore {
+public:
+    struct data { std::string game_id; TurnStruct actual_turn; };
+    std::optional<data> data_obj;
+};
+class ResponseSendGameInfoCommand : public LobbyServerMessageCore {
+public:
+    struct data { std::string game_id; TurnStruct actual_turn; };
+    std::optional<data> data_obj;
+};
+
+using AnyCommand = std::variant<
+    std::monostate,
+    SenderPingCommand, ResponsePingCommand,
+    SenderGetLobbyInfoCommand, ResponseGetLobbyInfoCommand,
+    SenderJoinGameCommand, ResponseJoinGameCommand,
+    SenderLeaveRoomCommand, ResponseLeaveRoomCommand,
+    SenderSendGameInfoCommand, ResponseSendGameInfoCommand
+>;
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ImageInfo, img_name, rotate, rotation, place, size)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(PlayerGameInfo, client_id, imgs, score, points, mistakes, rank)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ScoreEntry, client_id, score)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(PastTurn, turn_id, winner, winner_id, active)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(TurnStruct, turn_id, active, clients_data, clients_id_data, images_on_table, scoreboard, past_turns, winner_id, actual_imgs, players_imgs)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ActualGameStruct, game_id, game_name, players, max_players, nicknames, status)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ShortGameInfo, game_id, name)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ErrorStruct, code, message)
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SenderPingCommand::data, message)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ResponsePingCommand::data, message)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SenderGetLobbyInfoCommand::data, page)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ResponseGetLobbyInfoCommand::data, page, actual_games, next_page)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SenderJoinGameCommand::data, game_id, role)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ResponseJoinGameCommand::data, status, role, game_info)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SenderLeaveRoomCommand::data, message)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ResponseLeaveRoomCommand::data, message)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SenderSendGameInfoCommand::data, game_id, actual_turn)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ResponseSendGameInfoCommand::data, game_id, actual_turn)
+
+#define DEFINE_JSON_WITH_DATA_RENAME(Type) \
+    inline void to_json(json& j, const Type& p) { \
+        j = json{ {"command", p.command}, {"error", p.error} }; \
+        if(p.server_id) j["server_id"] = p.server_id; \
+        if(p.client_id) j["client_id"] = p.client_id; \
+        if(p.game_id) j["game_id"] = p.game_id; \
+        if(p.data_obj) j["data"] = p.data_obj; else j["data"] = nullptr; \
+    } \
+    inline void from_json(const json& j, Type& p) { \
+        j.at("command").get_to(p.command); \
+        if(j.contains("server_id")) j.at("server_id").get_to(p.server_id); \
+        if(j.contains("client_id")) j.at("client_id").get_to(p.client_id); \
+        if(j.contains("game_id")) j.at("game_id").get_to(p.game_id); \
+        if(j.contains("data") && !j["data"].is_null()) j.at("data").get_to(p.data_obj); \
+        if(j.contains("error") && !j["error"].is_null()) j.at("error").get_to(p.error); \
+    }
+
+DEFINE_JSON_WITH_DATA_RENAME(SenderPingCommand)
+DEFINE_JSON_WITH_DATA_RENAME(ResponsePingCommand)
+DEFINE_JSON_WITH_DATA_RENAME(SenderGetLobbyInfoCommand)
+DEFINE_JSON_WITH_DATA_RENAME(ResponseGetLobbyInfoCommand)
+DEFINE_JSON_WITH_DATA_RENAME(SenderJoinGameCommand)
+DEFINE_JSON_WITH_DATA_RENAME(ResponseJoinGameCommand)
+DEFINE_JSON_WITH_DATA_RENAME(SenderLeaveRoomCommand)
+DEFINE_JSON_WITH_DATA_RENAME(ResponseLeaveRoomCommand)
+DEFINE_JSON_WITH_DATA_RENAME(SenderSendGameInfoCommand)
+DEFINE_JSON_WITH_DATA_RENAME(ResponseSendGameInfoCommand)
