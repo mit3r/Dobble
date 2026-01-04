1123a15fb575d291f3707bbfc6b9c7a77f793903
patromi <patromi123@gmail.com>
2026-01-04 16:02:48 +0100

    fix: ensure INADDR_ANY is set for TCP client connection

--- src/server/common/BaseClient.cpp
+++ src/server/common/BaseClient.cpp
@@ -62,6 +62,7 @@ void TcpClient::connect() {
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port); 
+    addr.sin_addr.s_addr = INADDR_ANY;
     
     if (inet_pton(AF_INET, ip_address.c_str(), &addr.sin_addr) <= 0) {
         close(sock_fd);
